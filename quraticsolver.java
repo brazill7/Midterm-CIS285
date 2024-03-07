ff20f9c20ca59cd23484753863e2fb1e9193ad0d
mbraz <mbrazill@umich.edu>
2024-03-07 14:26:45 -0500

    second jenkins modification

--- quraticsolver.java
+++ quraticsolver.java
@@ -4,8 +4,8 @@ public class quraticsolver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//
-		int a = 1;
-		int b = 2;
+		int a = 3;
+		int b = 3;
 		int c = 3;
 
 		if (b*b - 4*a*c <0)
